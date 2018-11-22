3ae11e512698a5a786e1e8f35f9f7da87fe00cb0
pythonwhoissleepy <thisisnotarealemail@notreal.com>
2018-11-22 13:31:00 +1000

    basic player controls

---
+++ smack-project/Assets/Scripts/GameController.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour {
+
+    public bool paused;
+
+	// Use this for initialization
+	void Start () {
+	    paused = false;	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ smack-project/Assets/Scripts/Player/Player.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+
+    // stats
+    public int maxHp;
+    private int currentHp;
+
+    public int maxMana;
+    private int currentMana;
+    public float manaRestoreRate;
+
+    public float speed;
+
+    // physics, etc
+    private Rigidbody2D rb;
+    private GameController gameController;
+
+	// Use this for initialization
+	void Start () {
+		
+        gameController = GameObject.FindGameObjectWithTag("GameController").GetComponent<GameController>();
+        rb = GetComponent<Rigidbody2D>();
+
+	}
+	
+	void FixedUpdate () {
+	    
+        if (!gameController.paused) {
+                
+            float horiz = Input.GetAxisRaw("Horizontal");
+            float verti = Input.GetAxisRaw("Vertical");
+
+            if (horiz == 0 && verti == 0) {
+                rb.velocity = new Vector2(0, 0); 
+            } 
+            if (horiz == 1 || horiz == -1) {
+                rb.velocity = new Vector2(horiz * speed, 0);
+            }
+            if (verti == 1 || verti == -1) {
+                rb.velocity = new Vector2(0, verti * speed); 
+            }
+            // diagonal movement
+            if (horiz != 0 && verti != 0) {
+                rb.velocity = new Vector2(horiz * speed, verti * speed);
+            }
+
+            
+        }
+
+	}
+
+    void OnTriggerEnter2D(Collider2D other) {
+        
+        if (!gameController.paused) {
+            Debug.Log("collided with "+other.gameObject);
+        }
+    }
+}
