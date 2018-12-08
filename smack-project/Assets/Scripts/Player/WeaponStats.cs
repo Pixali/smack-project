6c2e3ba5a6fbfc0c2d7ed161f6665f9445899f47
Alwyn Wan <alwynwan@live.com.au>
2018-12-08 10:35:03 +1000

    Attached weapon object to player

---
+++ smack-project/Assets/Scripts/Player/WeaponStats.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Player
+{
+    public class WeaponStats
+    {
+        public string name { get; set; }
+        public float base_damage { get; set; }
+        public float range { get; set; }
+
+    }
+
+}
